7744171a746a1952688f50a3b9eafad01d145f33
Morgan Mueller <morgan.muellerb@gmail.com>
2025-05-21 22:21:04 -0400

    Delete utils directory

--- utils/interpolate.js
+++
@@ -1,6 +0,0 @@
-// utils/interpolate.js (recommended new file)
-
-export function interpolate(str, variables = {}) {
-    return str.replace(/{([^}]+)}/g, (_, key) => variables[key] ?? `{${key}}`);
-  }
-  
