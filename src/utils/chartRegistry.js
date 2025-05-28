9ec0507570a1d05947a3968afc7534ea21cf3efe
Morgan Mueller <mbm557@nyu.edu>
2025-05-28 13:19:41 -0400

    Add files via upload

--- src/utils/chartRegistry.js
+++ src/utils/chartRegistry.js
@@ -2,11 +2,14 @@ import SeasonalEDChart from "../components/charts/edSeasonalComparisonChart";
 import ExampleChart from "../components/charts/ExampleChart";
 import LineChart from "../components/charts/LineChart";
 import YearComparisonChart from "../components/charts/YearComparisonChart"
+import SmallMultipleBarChart from "../components/charts/SmallMultipleBarChart"
+
 const chartRegistry = {
   edSeasonalComparisonChart: SeasonalEDChart, 
   ExampleChart: ExampleChart, 
   lineChart: LineChart,
-  yearComparisonChart: YearComparisonChart
+  yearComparisonChart: YearComparisonChart,
+  smallMultipleBarChart: SmallMultipleBarChart
 
 };
 
