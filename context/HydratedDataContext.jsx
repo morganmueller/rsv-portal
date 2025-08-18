e6e1a2d3440685ad978749735b545161f285bf17
Morgan Mueller <morgan.muellerb@gmail.com>
2025-08-18 14:57:06 -0400

    Delete context directory

--- context/HydratedDataContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from "react";
-
-const HydratedDataContext = createContext({ data: {} });
-export const useHydratedData = () => useContext(HydratedDataContext);
-export default HydratedDataContext;
